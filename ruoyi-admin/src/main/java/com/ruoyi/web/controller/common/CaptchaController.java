1270b056d5630001accff9a010af85a49f89b841
wugh <328043740@qq.com>
2021-03-19 19:28:43 +0800

    [bug修复] 解决调整验证码干扰的类型无效,只用circle的bug [优化] 调整使用那种验证码的逻辑判断,使用switch优化,并减少重复代码

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
@@ -1,23 +1,25 @@
 package com.ruoyi.web.controller.common;
 
-import java.util.concurrent.TimeUnit;
-
-import cn.hutool.captcha.*;
+import cn.hutool.captcha.AbstractCaptcha;
+import cn.hutool.captcha.CircleCaptcha;
+import cn.hutool.captcha.LineCaptcha;
+import cn.hutool.captcha.ShearCaptcha;
 import cn.hutool.captcha.generator.CodeGenerator;
 import cn.hutool.captcha.generator.MathGenerator;
 import cn.hutool.captcha.generator.RandomGenerator;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
+import com.ruoyi.common.constant.Constants;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.redis.RedisCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.ruoyi.common.constant.Constants;
-import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.core.redis.RedisCache;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 验证码操作处理
@@ -27,94 +29,95 @@ import javax.annotation.Resource;
 @RestController
 public class CaptchaController {
 
-    // 圆圈干扰验证码
-    @Resource(name = "CircleCaptcha")
-    private CircleCaptcha circleCaptcha;
-    // 线段干扰的验证码
-    @Resource(name = "LineCaptcha")
-    private LineCaptcha lineCaptcha;
-    // 扭曲干扰验证码
-    @Resource(name = "ShearCaptcha")
-    private ShearCaptcha shearCaptcha;
+	// 圆圈干扰验证码
+	@Resource(name = "CircleCaptcha")
+	private CircleCaptcha circleCaptcha;
+	// 线段干扰的验证码
+	@Resource(name = "LineCaptcha")
+	private LineCaptcha lineCaptcha;
+	// 扭曲干扰验证码
+	@Resource(name = "ShearCaptcha")
+	private ShearCaptcha shearCaptcha;
 
-    @Autowired
-    private RedisCache redisCache;
+	@Autowired
+	private RedisCache redisCache;
 
-    // 验证码类型
-    @Value("${captcha.captchaType}")
-    private String captchaType;
-    // 验证码类别
-    @Value("${captcha.captchaCategory}")
-    private String captchaCategory;
-    // 数字验证码位数
-    @Value("${captcha.captchaNumberLength}")
-    private int numberLength;
-    // 字符验证码长度
-    @Value("${captcha.captchaCharLength}")
-    private int charLength;
+	// 验证码类型
+	@Value("${captcha.captchaType}")
+	private String captchaType;
+	// 验证码类别
+	@Value("${captcha.captchaCategory}")
+	private String captchaCategory;
+	// 数字验证码位数
+	@Value("${captcha.captchaNumberLength}")
+	private int numberLength;
+	// 字符验证码长度
+	@Value("${captcha.captchaCharLength}")
+	private int charLength;
 
-    /**
-     * 生成验证码
-     */
-    @GetMapping("/captchaImage")
-    public AjaxResult getCode() {
-        // 保存验证码信息
-        String uuid = IdUtil.simpleUUID();
-        String verifyKey = Constants.CAPTCHA_CODE_KEY + uuid;
-        String capStr = null, code = null;
-        // 生成验证码
-        CodeGenerator codeGenerator;
-        if ("math".equals(captchaType)) {
-            codeGenerator = new MathGenerator(numberLength);
-        } else if ("char".equals(captchaType)) {
-            codeGenerator = new RandomGenerator(charLength);
-        } else {
-            throw new IllegalArgumentException("验证码类型异常");
-        }
-        if ("line".equals(captchaCategory)) {
-            lineCaptcha.setGenerator(codeGenerator);
-            lineCaptcha.createCode();
-            capStr = lineCaptcha.getCode();
-        } else if ("circle".equals(captchaCategory)) {
-            circleCaptcha.setGenerator(codeGenerator);
-            circleCaptcha.createCode();
-            capStr = circleCaptcha.getCode();
-        } else if ("shear".equals(captchaCategory)) {
-            shearCaptcha.setGenerator(codeGenerator);
-            shearCaptcha.createCode();
-            capStr = shearCaptcha.getCode();
-        }  else {
-            throw new IllegalArgumentException("验证码类别异常");
-        }
-        if ("math".equals(captchaType)) {
-            code = getCodeResult(capStr);
-        } else if ("char".equals(captchaType)) {
-            code = capStr;
-        }
+	/**
+	 * 生成验证码
+	 */
+	@GetMapping("/captchaImage")
+	public AjaxResult getCode() {
+		// 保存验证码信息
+		String uuid = IdUtil.simpleUUID();
+		String verifyKey = Constants.CAPTCHA_CODE_KEY + uuid;
+		String code = null;
+		// 生成验证码
+		CodeGenerator codeGenerator;
+		AbstractCaptcha captcha;
+		switch (captchaType) {
+			case "math":
+				codeGenerator = new MathGenerator(numberLength);
+				break;
+			case "char":
+				codeGenerator = new RandomGenerator(charLength);
+				break;
+			default:
+				throw new IllegalArgumentException("验证码类型异常");
+		}
+		switch (captchaCategory) {
+			case "line":
+				captcha = lineCaptcha;
+				break;
+			case "circle":
+				captcha = circleCaptcha;
+				break;
+			case "shear":
+				captcha = shearCaptcha;
+				break;
+			default:
+				throw new IllegalArgumentException("验证码类别异常");
+		}
+		captcha.setGenerator(codeGenerator);
+		captcha.createCode();
+		if ("math".equals(captchaType)) {
+			code = getCodeResult(captcha.getCode());
+		} else if ("char".equals(captchaType)) {
+			code = captcha.getCode();
+		}
+		redisCache.setCacheObject(verifyKey, code, Constants.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
+		AjaxResult ajax = AjaxResult.success();
+		ajax.put("uuid", uuid);
+		ajax.put("img", captcha.getImageBase64());
+		return ajax;
+	}
 
-        redisCache.setCacheObject(verifyKey, code, Constants.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
-
-        circleCaptcha.createImage(capStr);
-        AjaxResult ajax = AjaxResult.success();
-        ajax.put("uuid", uuid);
-        ajax.put("img", circleCaptcha.getImageBase64());
-        return ajax;
-    }
-
-    private String getCodeResult(String capStr) {
-        int a = Convert.toInt(StrUtil.sub(capStr, 0, numberLength).trim());
-        char operator = capStr.charAt(numberLength);
-        int b = Convert.toInt(StrUtil.sub(capStr, numberLength + 1, numberLength + 1 + numberLength).trim());
-        switch (operator) {
-            case '*':
-                return a * b + "";
-            case '+':
-                return a + b + "";
-            case '-':
-                return a - b + "";
-            default:
-                return "";
-        }
-    }
+	private String getCodeResult(String capStr) {
+		int a = Convert.toInt(StrUtil.sub(capStr, 0, numberLength).trim());
+		char operator = capStr.charAt(numberLength);
+		int b = Convert.toInt(StrUtil.sub(capStr, numberLength + 1, numberLength + 1 + numberLength).trim());
+		switch (operator) {
+			case '*':
+				return a * b + "";
+			case '+':
+				return a + b + "";
+			case '-':
+				return a - b + "";
+			default:
+				return "";
+		}
+	}
 
 }
