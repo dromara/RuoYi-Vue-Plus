3e08a8e1c1ea923035c09af6cf1f802c1f6dd25b
疯狂的狮子li <15040126243@163.com>
2021-05-15 14:27:29 +0800

    update 通用Service接口 增加自定义vo转换函数

--- ruoyi-common/src/main/java/com/ruoyi/common/core/page/PagePlus.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/page/PagePlus.java
@@ -1,6 +1,5 @@
 package com.ruoyi.common.core.page;
 
-import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import lombok.Data;
@@ -10,34 +9,67 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @Accessors(chain = true)
 public class PagePlus<T,K> implements IPage<T> {
 
-    protected List<T> records;
-    protected List<K> recordsVo;
-    protected long total;
-    protected long size;
-    protected long current;
-    protected List<OrderItem> orders;
-    protected boolean optimizeCountSql;
-    protected boolean isSearchCount;
-    protected boolean hitCount;
-    protected String countId;
-    protected Long maxLimit;
+    /**
+     * domain实体列表
+     */
+    private List<T> records = Collections.emptyList();
+
+    /**
+     * vo实体列表
+     */
+    private List<K> recordsVo = Collections.emptyList();
+
+    /**
+     * 总数
+     */
+    private long total = 0L;
+
+    /**
+     * 页长度
+     */
+    private long size = 10L;
+
+    /**
+     * 当前页
+     */
+    private long current = 1L;
+
+    /**
+     * 排序字段信息
+     */
+    private List<OrderItem> orders = new ArrayList<>();
+
+    /**
+     * 自动优化 COUNT SQL
+     */
+    private boolean optimizeCountSql = true;
+
+    /**
+     * 是否进行 count 查询
+     */
+    private boolean isSearchCount = true;
+
+    /**
+     * 是否命中count缓存
+     */
+    private boolean hitCount = false;
+
+    /**
+     * countId
+     */
+    private String countId;
+
+    /**
+     * 最大limit
+     */
+    private Long maxLimit;
 
     public PagePlus() {
-        this.records = Collections.emptyList();
-        this.recordsVo = Collections.emptyList();
-        this.total = 0L;
-        this.size = 10L;
-        this.current = 1L;
-        this.orders = new ArrayList();
-        this.optimizeCountSql = true;
-        this.isSearchCount = true;
-        this.hitCount = false;
     }
 
     public PagePlus(long current, long size) {
@@ -53,18 +85,9 @@ public class PagePlus<T,K> implements IPage<T> {
     }
 
     public PagePlus(long current, long size, long total, boolean isSearchCount) {
-        this.records = Collections.emptyList();
-        this.total = 0L;
-        this.size = 10L;
-        this.current = 1L;
-        this.orders = new ArrayList();
-        this.optimizeCountSql = true;
-        this.isSearchCount = true;
-        this.hitCount = false;
         if (current > 1L) {
             this.current = current;
         }
-
         this.size = size;
         this.total = total;
         this.isSearchCount = isSearchCount;
@@ -76,12 +99,6 @@ public class PagePlus<T,K> implements IPage<T> {
 
     public boolean hasNext() {
         return this.current < this.getPages();
-    }
-
-    public void recordsToVo(Class<K> kClass) {
-        this.recordsVo = this.records.stream()
-                .map(any -> BeanUtil.toBean(any, kClass))
-                .collect(Collectors.toList());
     }
 
     @Override
@@ -116,7 +133,7 @@ public class PagePlus<T,K> implements IPage<T> {
 
     @Override
     public boolean isSearchCount() {
-        return this.total < 0L ? false : this.isSearchCount;
+        return this.total >= 0L && this.isSearchCount;
     }
 
     public PagePlus<T, K> setSearchCount(boolean isSearchCount) {
