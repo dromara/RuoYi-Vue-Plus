90063ad03c406c0cf88f5e6a1860bb43926bfddf
朱杰 <693337446@qq.com>
2021-05-31 17:23:13 +0800

    update 修改框架中批量接口，改为原生mybatis执行foreach，改善性能

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysUserRoleMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysUserRoleMapper.java
@@ -3,11 +3,21 @@ package com.ruoyi.system.mapper;
 import com.ruoyi.common.core.page.BaseMapperPlus;
 import com.ruoyi.system.domain.SysUserRole;
 
+import java.util.List;
+
 /**
  * 用户与角色关联表 数据层
  *
  * @author ruoyi
  */
 public interface SysUserRoleMapper extends BaseMapperPlus<SysUserRole> {
+
+	/**
+	 * 批量新增用户角色信息
+	 *
+	 * @param userRoleList 用户角色列表
+	 * @return 结果
+	 */
+	public int batchUserRole(List<SysUserRole> userRoleList);
 
 }
