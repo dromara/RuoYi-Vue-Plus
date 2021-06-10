57df2690da79e420b8e0510895cba8d5d903d8c3
疯狂的狮子li <15040126243@163.com>
2021-06-10 20:23:15 +0800

    update spring-cache 整合 redisson

--- ruoyi-demo/src/main/java/com/ruoyi/demo/controller/RedisLockController.java
+++ ruoyi-demo/src/main/java/com/ruoyi/demo/controller/RedisLockController.java
@@ -5,6 +5,7 @@ import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.redis.RedisLockManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,5 +62,14 @@ public class RedisLockController {
 			log.error(e.getMessage());
 		}
 		return AjaxResult.success();
+	}
+
+	/**
+	 * 测试spring-cache注解
+	 */
+	@Cacheable(value = "test", key = "#key")
+	@GetMapping("/testCache")
+	public AjaxResult<String> testCache(String key) {
+		return AjaxResult.success("操作成功", key);
 	}
 }
