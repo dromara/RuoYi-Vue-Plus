a0392865f4785a04eb59bb2cfd440e205b6e14d4
疯狂的狮子li <15040126243@163.com>
2021-06-16 19:24:26 +0800

    update 更新 feign demo 更清晰的用法

--- ruoyi-demo/src/main/java/com/ruoyi/demo/feign/FeignTestService.java
+++ ruoyi-demo/src/main/java/com/ruoyi/demo/feign/FeignTestService.java
@@ -1,5 +1,6 @@
 package com.ruoyi.demo.feign;
 
+import com.ruoyi.demo.feign.constant.FeignTestConstant;
 import com.ruoyi.demo.feign.fallback.FeignTestFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * feign测试service
- *
+ * 规范接口 Service 无感调用
+ * 常量管理请求路径 更加规范
+ * 自定义容错处理 安全可靠
  * @author Lion Li
  */
-@FeignClient(name = "baidu",url = "http://www.baidu.com",fallback = FeignTestFallback.class)
+@FeignClient(
+	name = FeignTestConstant.BAIDU_NAME,
+	url = FeignTestConstant.BAIDU_URL,
+	fallback = FeignTestFallback.class)
 public interface FeignTestService {
 
     @GetMapping("/s")
