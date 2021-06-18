281b6b6d2b20522d0a444c9126c25d6bd66aaf48
疯狂的狮子li <15040126243@163.com>
2021-06-18 15:25:37 +0800

    update 调整 core mp 包结构

--- ruoyi-common/src/main/java/com/ruoyi/common/core/mybatisplus/core/BaseMapperPlus.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/mybatisplus/core/BaseMapperPlus.java
@@ -1,6 +1,9 @@
-package com.ruoyi.common.core.page;
+package com.ruoyi.common.core.mybatisplus.core;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Collection;
 
 /**
  * 自定义 Mapper 接口, 实现 自定义扩展
@@ -9,4 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2021-05-13
  */
 public interface BaseMapperPlus<T> extends BaseMapper<T> {
+
+	int insertAll(@Param("list") Collection<T> batchList);
+
 }
