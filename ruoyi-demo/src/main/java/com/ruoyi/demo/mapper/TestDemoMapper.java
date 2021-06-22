a129fafb69fa9804ab960067021757ac6b835c97
疯狂的狮子li <15040126243@163.com>
2021-06-22 13:57:36 +0800

    update 更新多数据源与缓存 注意事项

--- ruoyi-demo/src/main/java/com/ruoyi/demo/mapper/TestDemoMapper.java
+++ ruoyi-demo/src/main/java/com/ruoyi/demo/mapper/TestDemoMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.CacheNamespace;
  * @author Lion Li
  * @date 2021-05-30
  */
+// 如使需切换数据源 请勿使用缓存 会造成数据不一致现象
 @CacheNamespace(implementation = MybatisPlusRedisCache.class, eviction = MybatisPlusRedisCache.class)
 public interface TestDemoMapper extends BaseMapperPlus<TestDemo> {
 
