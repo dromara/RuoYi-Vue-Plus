821f54f1ab984eadf3f7e0a2e79f9a372814f8d0
疯狂的狮子li <15040126243@163.com>
2021-07-08 20:05:37 +0800

    update 富文本接口返回值修正

--- ruoyi-ui/src/components/Editor/index.vue
+++ ruoyi-ui/src/components/Editor/index.vue
@@ -166,7 +166,7 @@ export default {
         // 获取光标所在位置
         let length = quill.getSelection().index;
         // 插入图片  res.url为服务器返回的图片地址
-        quill.insertEmbed(length, "image", process.env.VUE_APP_BASE_API + res.fileName);
+        quill.insertEmbed(length, "image", process.env.VUE_APP_BASE_API + res.data.fileName);
         // 调整光标到最后
         quill.setSelection(length + 1);
       } else {
