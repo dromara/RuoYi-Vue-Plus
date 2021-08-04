43901370e943d04d75e756e9938c5f14d915db7f
疯狂的狮子li <15040126243@163.com>
2021-08-04 18:13:11 +0800

    update 自动注入修改为强制注入 无用户信息返回 null 并日志报警

--- ruoyi-framework/src/main/java/com/ruoyi/framework/mybatisplus/CreateAndUpdateMetaObjectHandler.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/mybatisplus/CreateAndUpdateMetaObjectHandler.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.ruoyi.common.core.domain.model.LoginUser;
 import com.ruoyi.common.exception.CustomException;
 import com.ruoyi.common.utils.SecurityUtils;
-import com.ruoyi.common.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 
@@ -25,14 +24,10 @@ public class CreateAndUpdateMetaObjectHandler implements MetaObjectHandler {
 		try {
 			//根据属性名字设置要填充的值
 			if (metaObject.hasGetter("createTime")) {
-				if (StringUtils.isEmpty(metaObject.getValue("createTime"))) {
-					this.setFieldValByName("createTime", new Date(), metaObject);
-				}
+				this.setFieldValByName("createTime", new Date(), metaObject);
 			}
 			if (metaObject.hasGetter("createBy")) {
-				if (StringUtils.isEmpty(metaObject.getValue("createBy"))) {
-					this.setFieldValByName("createBy", getLoginUsername(), metaObject);
-				}
+				this.setFieldValByName("createBy", getLoginUsername(), metaObject);
 			}
 		} catch (Exception e) {
 			throw new CustomException("自动注入异常 => " + e.getMessage(), HttpStatus.HTTP_UNAUTHORIZED);
@@ -43,14 +38,10 @@ public class CreateAndUpdateMetaObjectHandler implements MetaObjectHandler {
 	public void updateFill(MetaObject metaObject) {
 		try {
 			if (metaObject.hasGetter("updateBy")) {
-				if (StringUtils.isEmpty(metaObject.getValue("updateBy"))) {
-					this.setFieldValByName("updateBy", getLoginUsername(), metaObject);
-				}
+				this.setFieldValByName("updateBy", getLoginUsername(), metaObject);
 			}
 			if (metaObject.hasGetter("updateTime")) {
-				if (StringUtils.isEmpty(metaObject.getValue("updateTime"))) {
-					this.setFieldValByName("updateTime", new Date(), metaObject);
-				}
+				this.setFieldValByName("updateTime", new Date(), metaObject);
 			}
 		} catch (Exception e) {
 			throw new CustomException("自动注入异常 => " + e.getMessage(), HttpStatus.HTTP_UNAUTHORIZED);
@@ -65,7 +56,7 @@ public class CreateAndUpdateMetaObjectHandler implements MetaObjectHandler {
 		try {
 			loginUser = SecurityUtils.getLoginUser();
 		} catch (Exception e) {
-			log.warn("自动注入警告 => 用户未登录");
+			log.error("自动注入警告 => 用户未登录");
 			return null;
 		}
 		return loginUser.getUsername();
