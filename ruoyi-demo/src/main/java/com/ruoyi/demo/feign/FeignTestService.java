237b598264ba6133215764fe74def21e5a3033b1
疯狂的狮子li <15040126243@163.com>
2021-09-03 11:43:34 +0800

    update 更新feign关于熔断注释

--- ruoyi-demo/src/main/java/com/ruoyi/demo/feign/FeignTestService.java
+++ ruoyi-demo/src/main/java/com/ruoyi/demo/feign/FeignTestService.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * feign测试service
  * 规范接口 Service 无感调用
  * 常量管理请求路径 更加规范
- * 自定义容错处理 安全可靠
+ * 自定义容错处理 安全可靠 (需自行配置熔断器)
+ * 增加 feign 的目的为使 http 请求接口化
+ *
  * @author Lion Li
  */
 @FeignClient(
