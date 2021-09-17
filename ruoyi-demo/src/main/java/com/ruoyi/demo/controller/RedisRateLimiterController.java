2569e8c0a4e960c8eb34c56985dba9608f7e15ea
疯狂的狮子li <15040126243@163.com>
2021-09-17 17:22:55 +0800

    add 增加 集群限流演示案例

--- ruoyi-demo/src/main/java/com/ruoyi/demo/controller/RedisRateLimiterController.java
+++ ruoyi-demo/src/main/java/com/ruoyi/demo/controller/RedisRateLimiterController.java
@@ -2,6 +2,7 @@ package com.ruoyi.demo.controller;
 
 import com.ruoyi.common.annotation.RateLimiter;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.enums.LimitType;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -22,12 +23,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisRateLimiterController {
 
 	/**
-	 * 测试限流注解
+	 * 测试全局限流
+	 * 全局影响
 	 */
-	@ApiOperation("测试限流注解")
+	@ApiOperation("测试全局限流")
 	@RateLimiter(count = 2, time = 10)
 	@GetMapping("/test")
 	public  AjaxResult<String> test(String value){
+		return AjaxResult.success("操作成功",value);
+	}
+
+	/**
+	 * 测试请求IP限流
+	 * 同一IP请求受影响
+	 */
+	@ApiOperation("测试请求IP限流")
+	@RateLimiter(count = 2, time = 10, limitType = LimitType.IP)
+	@GetMapping("/testip")
+	public  AjaxResult<String> testip(String value){
+		return AjaxResult.success("操作成功",value);
+	}
+
+	/**
+	 * 测试集群实例限流
+	 * 启动两个后端服务互不影响
+	 */
+	@ApiOperation("测试集群实例限流")
+	@RateLimiter(count = 2, time = 10, limitType = LimitType.CLUSTER)
+	@GetMapping("/testcluster")
+	public  AjaxResult<String> testcluster(String value){
 		return AjaxResult.success("操作成功",value);
 	}
 
