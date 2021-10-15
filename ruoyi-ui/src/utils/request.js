bb43b2853d956cba57572df8d01da8e3ac0a201d
疯狂的狮子li <15040126243@163.com>
2021-10-15 11:45:43 +0800

    update 前端增加默认国际化参数

--- ruoyi-ui/src/utils/request.js
+++ ruoyi-ui/src/utils/request.js
@@ -5,6 +5,8 @@ import { getToken } from '@/utils/auth'
 import errorCode from '@/utils/errorCode'
 
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
+// 对应国际化资源文件后缀
+axios.defaults.headers['Content-Language'] = 'zh_CN'
 // 创建axios实例
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
