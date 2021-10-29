25321984fad576dad314864a45a7f997b278ded0
sxq <812980466@qq.com>
2021-10-29 16:55:42 +0800

    新增startup trace监控。

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -2,6 +2,7 @@ package com.ruoyi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 /**
  * 启动程序
@@ -14,7 +15,11 @@ public class RuoYiApplication {
 
     public static void main(String[] args) {
         System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
+        System.setProperty("StartFlightRecording:filename", "recording.jfr");
+        System.setProperty("duration", "10s");
+        SpringApplication application = new SpringApplication(RuoYiApplication.class);
+          application.setApplicationStartup(new BufferingApplicationStartup(2048));
+          application.run(args);
         System.out.println("(♥◠‿◠)ﾉﾞ  RuoYi-Vue-Plus启动成功   ლ(´ڡ`ლ)ﾞ");
     }
 
