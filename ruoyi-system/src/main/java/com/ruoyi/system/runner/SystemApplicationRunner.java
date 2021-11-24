1f3ef4ffe18d3590944c7e0a8c5c827ea414f7bd
疯狂的狮子li <15040126243@163.com>
2021-11-24 10:59:18 +0800

    update 初始化数据转移到 ApplicationRunner 统一处理

--- ruoyi-system/src/main/java/com/ruoyi/system/runner/SystemApplicationRunner.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/runner/SystemApplicationRunner.java
@@ -11,6 +11,11 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * 初始化 system 模块对应业务数据
+ *
+ * @author Lion Li
+ */
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Component
