2fe8291f1df0795490464118c3a6a56d72b205d4
疯狂的狮子li <15040126243@163.com>
2021-11-26 13:40:52 +0800

    update 修改 LOGIN_TOKEN_KEY 内容 对应 satoken 的 key

--- ruoyi-common/src/main/java/com/ruoyi/common/constant/Constants.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/Constants.java
@@ -64,7 +64,7 @@ public class Constants {
     /**
      * 登录用户 redis key
      */
-    public static final String LOGIN_TOKEN_KEY = "login_tokens:";
+    public static final String LOGIN_TOKEN_KEY = "Authorization:login:token:";
 
     /**
      * 在线用户 redis key
