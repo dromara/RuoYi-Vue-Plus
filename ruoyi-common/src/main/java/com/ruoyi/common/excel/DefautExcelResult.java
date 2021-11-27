e11b7275841fe2263fa1146394f146ac43ddc7de
疯狂的狮子Li <15040126243@163.com>
2021-11-27 12:43:34 +0800

    update 修改移除无用回执信息

--- ruoyi-common/src/main/java/com/ruoyi/common/excel/DefautExcelResult.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/excel/DefautExcelResult.java
@@ -1,6 +1,5 @@
 package com.ruoyi.common.excel;
 
-import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import lombok.Setter;
 
@@ -64,15 +63,11 @@ public class DefautExcelResult<T> implements ExcelResult<T> {
         if (successCount == 0) {
             return "读取失败，未解析到数据";
         } else {
-            if (errorList.size() == 0) {
+            if (errorCount == 0) {
                 return StrUtil.format("恭喜您，全部读取成功！共{}条", successCount);
             } else {
-                return StrUtil.format("部分读取成功，其中成功{}条，失败{}条，错误信息如下：<br/>{}",
-                    successCount,
-                    errorCount,
-                    CollUtil.join(errorList, "<br/>"));
+                return "";
             }
-
         }
     }
 }
