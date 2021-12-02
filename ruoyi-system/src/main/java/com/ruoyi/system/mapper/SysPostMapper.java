209654fdff7206c2638d9fbe26eb0fb17a68adb4
zendwang <wangzhenxian@idadt.com>
2021-12-02 20:13:47 +0800

    fix selectPostListByUserId方法出参调整为Long

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysPostMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysPostMapper.java
@@ -18,7 +18,7 @@ public interface SysPostMapper extends BaseMapperPlus<SysPost> {
      * @param userId 用户ID
      * @return 选中岗位ID列表
      */
-    List<Integer> selectPostListByUserId(Long userId);
+    List<Long> selectPostListByUserId(Long userId);
 
     /**
      * 查询用户所属岗位组
