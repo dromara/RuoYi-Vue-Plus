cb713e32d3caa270e6dd0d411edbddf6e6bb21f3
疯狂的狮子li <15040126243@163.com>
2022-02-16 16:54:19 +0800

    fix 修复 insertOrUpdateBatch 获取模板错误

--- ruoyi-common/src/main/java/com/ruoyi/common/core/mapper/BaseMapperPlus.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/mapper/BaseMapperPlus.java
@@ -108,7 +108,7 @@ public interface BaseMapperPlus<M, T, V> extends BaseMapper<T> {
         Assert.notEmpty(keyProperty, "error: can not execute. because can not find column for id from entity!");
         return SqlHelper.saveOrUpdateBatch(this.currentModelClass(), this.currentMapperClass(), log, entityList, batchSize, (sqlSession, entity) -> {
             Object idVal = tableInfo.getPropertyValue(entity, keyProperty);
-            String sqlStatement = SqlHelper.getSqlStatement(this.currentMapperClass(), SqlMethod.INSERT_ONE);
+            String sqlStatement = SqlHelper.getSqlStatement(this.currentMapperClass(), SqlMethod.SELECT_BY_ID);
             return StringUtils.checkValNull(idVal)
                 || CollectionUtils.isEmpty(sqlSession.selectList(sqlStatement, entity));
         }, (sqlSession, entity) -> {
