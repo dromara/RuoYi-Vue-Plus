f41bfdb8eba36ab3d38c11fff2328e6c11888d4b
疯狂的狮子Li <15040126243@163.com>
2022-03-03 01:55:01 +0800

    update 更新数据权限Service的使用说明

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysDataScopeServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysDataScopeServiceImpl.java
@@ -14,6 +14,14 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 数据权限 实现
+ * <p>
+ * 注意: 此Service内不允许调用标注`数据权限`注解的方法
+ * 例如: deptMapper.selectList 此 selectList 方法标注了`数据权限`注解 会出现循环解析的问题
+ *
+ * @author Lion Li
+ */
 @RequiredArgsConstructor
 @Service("sdss")
 public class SysDataScopeServiceImpl implements ISysDataScopeService {
