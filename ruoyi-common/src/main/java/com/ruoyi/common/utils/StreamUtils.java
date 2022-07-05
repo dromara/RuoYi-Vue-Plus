9cd4f0c332143e996fd6c33aec3c9d3ab2ea8a1d
疯狂的狮子li <15040126243@163.com>
2022-07-05 11:03:28 +0800

    update 补全 StreamUtils 类注释

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/StreamUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/StreamUtils.java
@@ -2,12 +2,20 @@ package com.ruoyi.common.utils;
 
 import cn.hutool.core.collection.CollStreamUtil;
 import cn.hutool.core.collection.CollUtil;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * stream 流工具类
+ *
+ * @author Lion Li
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StreamUtils extends CollStreamUtil {
 
     public static <E> List<E> filter(Collection<E> collection, Predicate<E> function) {
