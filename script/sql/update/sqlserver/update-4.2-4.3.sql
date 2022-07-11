f2d502faf8c23ad940762493b31d759c5dfd6bc4
疯狂的狮子li <15040126243@163.com>
2022-07-11 16:21:20 +0800

    fix 修复 sqlserver 更新sql错误提交

--- script/sql/update/sqlserver/update-4.2-4.3.sql
+++ script/sql/update/sqlserver/update-4.2-4.3.sql
@@ -1,1 +1,2 @@
-insert into sys_menu values('112',  '缓存列表', '2',   '6', 'cacheList',  'monitor/cache/list',       '', '1', '0', 'C', '0', '0', 'monitor:cache:list',      'redis-list',    'admin', now(), '', null, '缓存列表菜单');
+INSERT [sys_menu] ([menu_id], [menu_name], [parent_id], [order_num], [path], [component], [query_param], [is_frame], [is_cache], [menu_type], [visible], [status], [perms], [icon], [create_by], [create_time], [update_by], [update_time], [remark]) VALUES (112, N'缓存列表', 2, 6, N'cacheList', N'monitor/cache/list', N'', 1, 0, N'C', N'0', N'0', N'monitor:cache:list', N'redis-list', N'admin', getdate(), N'', NULL, N'缓存列表菜单')
+GO
