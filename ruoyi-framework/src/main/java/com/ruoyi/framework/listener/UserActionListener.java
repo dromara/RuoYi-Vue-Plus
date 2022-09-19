513bda53cd54051593b52b58885f80c8524fa0b7
疯狂的狮子li <15040126243@163.com>
2022-09-19 18:18:01 +0800

    fix 修复 在线用户设置永不过期 超时时间-1推送redis无效问题

--- ruoyi-framework/src/main/java/com/ruoyi/framework/listener/UserActionListener.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/listener/UserActionListener.java
@@ -50,7 +50,11 @@ public class UserActionListener implements SaTokenListener {
             dto.setTokenId(tokenValue);
             dto.setUserName(user.getUsername());
             dto.setDeptName(user.getDeptName());
-            RedisUtils.setCacheObject(CacheConstants.ONLINE_TOKEN_KEY + tokenValue, dto, Duration.ofSeconds(tokenConfig.getTimeout()));
+            if(tokenConfig.getTimeout() == -1) {
+                RedisUtils.setCacheObject(CacheConstants.ONLINE_TOKEN_KEY + tokenValue, dto);
+            } else {
+                RedisUtils.setCacheObject(CacheConstants.ONLINE_TOKEN_KEY + tokenValue, dto, Duration.ofSeconds(tokenConfig.getTimeout()));
+            }
             log.info("user doLogin, userId:{}, token:{}", loginId, tokenValue);
         } else if (userType == UserType.APP_USER) {
             // app端 自行根据业务编写
