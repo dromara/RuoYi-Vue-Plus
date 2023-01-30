e94ed91f0b31b4fba6fec17168c8c5e6d9a9604d
疯狂的狮子Li <15040126243@163.com>
2023-01-30 23:01:14 +0800

    remove 删除 4.X 相关更新sql文件

--- script/sql/update/postgres/update-4.3-4.4.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "sys_oss_config" ADD COLUMN "access_policy" char(1) NOT NULL DEFAULT '1'::bpchar;
-
-COMMENT ON COLUMN "sys_oss_config"."access_policy" IS '桶权限类型(0=private 1=public 2=custom)';
--- script/sql/update/update-4.3-4.4.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE sys_oss_config ADD COLUMN access_policy char(1) NOT NULL DEFAULT 1 COMMENT '桶权限类型(0=private 1=public 2=custom)' AFTER region;
