9e061669b1bde55a451e387e94be1a57fa30cbbe
mroldx <974751082@qq.com>
2023-05-24 14:25:16 +0800

    update:更新Mapstruct参考文档链接

--- ruoyi-common/ruoyi-common-core/src/main/java/org/dromara/common/core/utils/MapstructUtils.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/org/dromara/common/core/utils/MapstructUtils.java
@@ -12,7 +12,8 @@ import java.util.Map;
 
 /**
  * Mapstruct 工具类
- * <p>参考文档：<a href="https://mapstruct.plus/guide/quick-start">mapstruct-plus</a></p>
+ * <p>参考文档：<a href="https://mapstruct.plus/introduction/quick-start.html">mapstruct-plus</a></p>
+ *
  *
  * @author Michelle.Chung
  */
