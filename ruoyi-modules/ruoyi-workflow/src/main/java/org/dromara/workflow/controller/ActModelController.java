1ce0e61aba99fbd104e6a8864d299741fea46eb1
gssong <1742057357@qq.com>
2023-06-04 14:15:06 +0800

    修改画图账户登录信息

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/controller/ActModelController.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/controller/ActModelController.java
@@ -10,6 +10,7 @@ import org.dromara.common.json.utils.JsonUtils;
 import org.dromara.common.log.annotation.Log;
 import org.dromara.common.log.enums.BusinessType;
 import org.dromara.common.mybatis.core.page.TableDataInfo;
+import org.dromara.common.satoken.utils.LoginHelper;
 import org.dromara.common.web.core.BaseController;
 import org.dromara.workflow.domain.bo.ModelBo;
 import org.dromara.workflow.domain.vo.AccountVo;
@@ -20,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 /**
  * 模型管理 控制层
@@ -48,12 +51,11 @@ public class ActModelController extends BaseController {
      */
     @GetMapping("/rest/account")
     public String getAccount() {
-
         AccountVo accountVo = new AccountVo();
-        accountVo.setId("admin");
+        accountVo.setId(Objects.requireNonNull(LoginHelper.getUserId()).toString());
         accountVo.setFirstName("");
-        accountVo.setLastName("admin");
-        accountVo.setFullName("admin");
+        accountVo.setLastName(LoginHelper.getUsername());
+        accountVo.setFullName(LoginHelper.getUsername());
         return JsonUtils.toJsonString(accountVo);
     }
 
@@ -88,7 +90,7 @@ public class ActModelController extends BaseController {
     @Log(title = "模型管理", businessType = BusinessType.UPDATE)
     @PostMapping(value = "/rest/models/{modelId}/editor/json")
     public R<Void> editModel(@PathVariable String modelId, @RequestParam MultiValueMap<String, String> values) {
-        return toAjax(iActModelService.editModel(modelId,values));
+        return toAjax(iActModelService.editModel(modelId, values));
     }
 
     /**
