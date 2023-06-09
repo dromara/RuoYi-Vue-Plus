0b79ada24c9ba00fcd02e841ef53d108841d236c
疯狂的狮子Li <15040126243@163.com>
2023-06-09 11:18:10 +0800

    update 优化 注释掉暂时用不上的类

--- ruoyi-common/ruoyi-common-sms/src/main/java/org/dromara/common/sms/config/SmsConfig.java
+++ ruoyi-common/ruoyi-common-sms/src/main/java/org/dromara/common/sms/config/SmsConfig.java
@@ -1,8 +1,6 @@
 package org.dromara.common.sms.config;
 
-import org.dromara.common.sms.config.properties.SmsProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * 短信配置类
@@ -11,7 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  * @version 4.2.0
  */
 @AutoConfiguration
-@EnableConfigurationProperties(SmsProperties.class)
+//@EnableConfigurationProperties(SmsProperties.class)
 public class SmsConfig {
 
 }
