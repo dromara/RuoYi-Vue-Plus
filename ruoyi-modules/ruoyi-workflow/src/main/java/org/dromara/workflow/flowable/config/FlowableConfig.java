bfe04233716902dbbe946187fb7f88855eee4353
gssong <1742057357@qq.com>
2023-08-06 21:01:19 +0800

    add 添加引擎调度监听

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/flowable/config/FlowableConfig.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/flowable/config/FlowableConfig.java
@@ -3,7 +3,11 @@ package org.dromara.workflow.flowable.config;
 import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import org.flowable.spring.SpringProcessEngineConfiguration;
 import org.flowable.spring.boot.EngineConfigurationConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
 
 /**
  * flowable配置
@@ -13,8 +17,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
 
+    @Autowired
+    private GlobalFlowableListener globalFlowableListener;
     @Override
     public void configure(SpringProcessEngineConfiguration processEngineConfiguration) {
         processEngineConfiguration.setIdGenerator(IdWorker::getIdStr);
+        processEngineConfiguration.setEventListeners(Collections.singletonList(globalFlowableListener));
     }
 }
