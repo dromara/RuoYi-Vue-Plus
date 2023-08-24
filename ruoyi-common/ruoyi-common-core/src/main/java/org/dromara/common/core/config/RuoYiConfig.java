c1b9b95581f63a14d941731467e4ce89305d2221
疯狂的狮子Li <15040126243@163.com>
2023-08-24 19:45:08 +0800

    remove 删除无用配置

--- ruoyi-common/ruoyi-common-core/src/main/java/org/dromara/common/core/config/RuoYiConfig.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/org/dromara/common/core/config/RuoYiConfig.java
@@ -1,7 +1,6 @@
 package org.dromara.common.core.config;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -30,20 +29,5 @@ public class RuoYiConfig {
      * 版权年份
      */
     private String copyrightYear;
-
-    /**
-     * 实例演示开关
-     */
-    private boolean demoEnabled;
-
-    /**
-     * 获取地址开关
-     */
-    @Getter
-    private static boolean addressEnabled;
-
-    public void setAddressEnabled(boolean addressEnabled) {
-        RuoYiConfig.addressEnabled = addressEnabled;
-    }
 
 }
