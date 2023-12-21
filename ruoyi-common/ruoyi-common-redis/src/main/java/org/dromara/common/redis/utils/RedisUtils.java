521596bc12863f7a87a8a07ce81f18151e962a0c
疯狂的狮子Li <15040126243@163.com>
2023-12-21 13:09:09 +0800

    update 优化 丰富RedisUtils对List Set类型的操作

--- ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/utils/RedisUtils.java
+++ ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/utils/RedisUtils.java
@@ -255,6 +255,18 @@ public class RedisUtils {
     }
 
     /**
+     * 追加缓存List数据
+     *
+     * @param key  缓存的键值
+     * @param data 待缓存的数据
+     * @return 缓存的对象
+     */
+    public static <T> boolean addCacheList(final String key, final T data) {
+        RList<T> rList = CLIENT.getList(key);
+        return rList.add(data);
+    }
+
+    /**
      * 注册List监听器
      * <p>
      * key 监听器需开启 `notify-keyspace-events` 等 redis 相关配置
@@ -279,6 +291,19 @@ public class RedisUtils {
     }
 
     /**
+     * 获得缓存的list对象(范围)
+     *
+     * @param key  缓存的键值
+     * @param form 起始下标
+     * @param to   截止下标
+     * @return 缓存键值对应的数据
+     */
+    public static <T> List<T> getCacheListRange(final String key, int form, int to) {
+        RList<T> rList = CLIENT.getList(key);
+        return rList.range(form, to);
+    }
+
+    /**
      * 缓存Set
      *
      * @param key     缓存键值
@@ -288,6 +313,18 @@ public class RedisUtils {
     public static <T> boolean setCacheSet(final String key, final Set<T> dataSet) {
         RSet<T> rSet = CLIENT.getSet(key);
         return rSet.addAll(dataSet);
+    }
+
+    /**
+     * 追加缓存Set数据
+     *
+     * @param key  缓存的键值
+     * @param data 待缓存的数据
+     * @return 缓存的对象
+     */
+    public static <T> boolean addCacheSet(final String key, final T data) {
+        RSet<T> rSet = CLIENT.getSet(key);
+        return rSet.add(data);
     }
 
     /**
