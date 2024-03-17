6309af9db8e166c17e3d8b4eb4298ccfaa34e80b
疯狂的狮子Li <15040126243@163.com>
2024-03-17 23:26:13 +0800

    update easy-retry 3.1.0 => 3.1.1 增加日志自动推送配置

--- ruoyi-common/ruoyi-common-job/src/main/java/org/dromara/common/job/config/EasyRetryConfig.java
+++ ruoyi-common/ruoyi-common-job/src/main/java/org/dromara/common/job/config/EasyRetryConfig.java
@@ -1,8 +1,15 @@
 package org.dromara.common.job.config;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.aizuda.easy.retry.client.common.appender.EasyRetryLogbackAppender;
+import com.aizuda.easy.retry.client.common.event.EasyRetryStartingEvent;
 import com.aizuda.easy.retry.client.starter.EnableEasyRetry;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -16,4 +23,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableEasyRetry(group = "${easy-retry.group-name}")
 public class EasyRetryConfig {
+
+    @EventListener(EasyRetryStartingEvent.class)
+    public void onStarting(EasyRetryStartingEvent event) {
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        EasyRetryLogbackAppender<ILoggingEvent> ca = new EasyRetryLogbackAppender<>();
+        ca.setName("easy_log_appender");
+        ca.start();
+        Logger rootLogger = lc.getLogger(Logger.ROOT_LOGGER_NAME);
+        rootLogger.addAppender(ca);
+    }
+
 }
