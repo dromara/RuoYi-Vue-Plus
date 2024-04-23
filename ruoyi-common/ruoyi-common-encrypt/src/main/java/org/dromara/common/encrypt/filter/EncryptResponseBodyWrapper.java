748411c8dbd570928bb0dc021b5a6525e7c3c510
疯狂的狮子Li <15040126243@163.com>
2024-04-23 11:36:23 +0800

    fix 修复 部分浏览器无法获取加密响应头问题

--- ruoyi-common/ruoyi-common-encrypt/src/main/java/org/dromara/common/encrypt/filter/EncryptResponseBodyWrapper.java
+++ ruoyi-common/ruoyi-common-encrypt/src/main/java/org/dromara/common/encrypt/filter/EncryptResponseBodyWrapper.java
@@ -76,6 +76,7 @@ public class EncryptResponseBodyWrapper extends HttpServletResponseWrapper {
         String encryptPassword = EncryptUtils.encryptByRsa(encryptAes, publicKey);
 
         // 设置响应头
+        servletResponse.addHeader("Access-Control-Expose-Headers", headerFlag);
         servletResponse.setHeader(headerFlag, encryptPassword);
         servletResponse.setHeader("Access-Control-Allow-Origin", "*");
         servletResponse.setHeader("Access-Control-Allow-Methods", "*");
