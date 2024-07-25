52b0fa9a5482a2278b0c683dab7d130a9e7d8b87
AprilWind <2100166581@qq.com>
2024-07-25 10:00:10 +0800

    update 优化Monitor监控服务通知分类（可扩展邮箱发送，短信发送）

--- ruoyi-extend/ruoyi-monitor-admin/src/main/java/org/dromara/monitor/admin/notifier/CustomNotifier.java
+++ ruoyi-extend/ruoyi-monitor-admin/src/main/java/org/dromara/monitor/admin/notifier/CustomNotifier.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import static de.codecentric.boot.admin.server.domain.values.StatusInfo.*;
+
 /**
  * 自定义事件通知处理
  *
@@ -28,13 +30,55 @@ public class CustomNotifier extends AbstractEventNotifier {
         return Mono.fromRunnable(() -> {
             // 实例状态改变事件
             if (event instanceof InstanceStatusChangedEvent) {
+                // 获取实例注册名称
                 String registName = instance.getRegistration().getName();
+                // 获取实例ID
                 String instanceId = event.getInstance().getValue();
+                // 获取实例状态
                 String status = ((InstanceStatusChangedEvent) event).getStatusInfo().getStatus();
-                log.info("Instance Status Change: [{}],[{}],[{}]", registName, instanceId, status);
-            }
+                // 获取服务URL
+                String serviceUrl = instance.getRegistration().getServiceUrl();
+                String statusName = null;
 
+                // 根据实例状态执行相应的操作
+                switch (status) {
+                    // 服务上线（实例成功启动并可以正常处理请求）
+                    case STATUS_UP:
+                        statusName = "服务上线";
+                        break;
+
+                    // 服务离线（实例被手动或自动地从服务中移除）
+                    case STATUS_OFFLINE:
+                        statusName = "服务离线";
+                        break;
+
+                    // 服务受限（表示实例在某些方面受限，可能无法完全提供所有服务）
+                    case STATUS_RESTRICTED:
+                        statusName = "服务受限";
+                        break;
+
+                    // 停止服务状态（表示实例已被标记为停止提供服务，可能是计划内维护或测试）
+                    case STATUS_OUT_OF_SERVICE:
+                        statusName = "停止服务状态";
+                        break;
+
+                    // 服务下线（实例因崩溃、错误或其他原因停止运行）
+                    case STATUS_DOWN:
+                        statusName = "服务下线";
+                        break;
+
+                    // 服务未知异常（监控系统无法确定实例的当前状态）
+                    case STATUS_UNKNOWN:
+                        statusName = "服务未知异常";
+                        break;
+
+                    // 默认情况（没有匹配的状态）
+                    default:
+                        break;
+                }
+                log.info("Instance Status Change: 状态名称【{}】, 注册名称【{}】, 实例ID【{}】, 状态【{}】, 服务URL【{}】",
+                    statusName, registName, instanceId, status, serviceUrl);
+            }
         });
     }
-
 }
