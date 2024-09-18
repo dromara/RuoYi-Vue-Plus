74af811a3b856277ac125b15b5e66cbc5fae37d8
疯狂的狮子Li <15040126243@163.com>
2024-09-18 18:07:28 +0800

    update 优化 删除掉有问题的方法(使用RedisUtils)

--- ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/utils/CacheUtils.java
+++ ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/utils/CacheUtils.java
@@ -1,35 +1,21 @@
 package org.dromara.common.redis.utils;
 
-import org.dromara.common.core.utils.SpringUtils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.redisson.api.RMap;
+import org.dromara.common.core.utils.SpringUtils;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 
-import java.util.Set;
-
 /**
- * 缓存操作工具类 {@link }
+ * 缓存操作工具类
  *
  * @author Michelle.Chung
- * @date 2022/8/13
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @SuppressWarnings(value = {"unchecked"})
 public class CacheUtils {
 
     private static final CacheManager CACHE_MANAGER = SpringUtils.getBean(CacheManager.class);
-
-    /**
-     * 获取缓存组内所有的KEY
-     *
-     * @param cacheNames 缓存组名称
-     */
-    public static Set<Object> keys(String cacheNames) {
-        RMap<Object, Object> rmap = (RMap<Object, Object>) CACHE_MANAGER.getCache(cacheNames).getNativeCache();
-        return rmap.keySet();
-    }
 
     /**
      * 获取缓存值
