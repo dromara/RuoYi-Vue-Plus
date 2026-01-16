befabc61de1ad0f4b51fc70a0ec8cb6041a18d04
疯狂的狮子Li <15040126243@163.com>
2026-01-16 15:42:02 +0800

    update 优化 mqtt-server 文档搭建说明

--- ruoyi-common/ruoyi-common-mqtt/src/main/java/org/dromara/common/mqtt/config/MqttAutoConfiguration.java
+++ ruoyi-common/ruoyi-common-mqtt/src/main/java/org/dromara/common/mqtt/config/MqttAutoConfiguration.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Bean;
  * mqtt客户端配置初始化
  * <p>
  * 用法文档 <a href="https://gitee.com/dromara/mica-mqtt/blob/master/starter/mica-mqtt-client-spring-boot-starter/README.md">...</a>
+ * 测试server搭建:
+ * 可执行下载其他mqtt服务端搭建
+ * 也可使用 mica自带的server搭建 <a href="https://gitee.com/dromara/mica-mqtt/blob/master/starter/mica-mqtt-server-spring-boot-starter/README.md">...</a>
  *
  * @author Lion Li
  */
--- ruoyi-modules/ruoyi-demo/src/main/java/org/dromara/demo/controller/MqttController.java
+++ ruoyi-modules/ruoyi-demo/src/main/java/org/dromara/demo/controller/MqttController.java
@@ -18,6 +18,9 @@ import java.nio.charset.StandardCharsets;
  * mqtt 演示案例
  * <p>
  * 用法文档 <a href="https://gitee.com/dromara/mica-mqtt/blob/master/starter/mica-mqtt-client-spring-boot-starter/README.md">...</a>
+ * 测试server搭建:
+ * 可执行下载其他mqtt服务端搭建
+ * 也可使用 mica自带的server搭建 <a href="https://gitee.com/dromara/mica-mqtt/blob/master/starter/mica-mqtt-server-spring-boot-starter/README.md">...</a>
  *
  * @author Lion Li
  */
