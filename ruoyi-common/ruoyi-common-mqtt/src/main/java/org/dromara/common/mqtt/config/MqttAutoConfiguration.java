3b5d7eba37da2524edbb26c878c7da02a9804b97
疯狂的狮子Li <15040126243@163.com>
2026-02-10 16:32:50 +0800

    update 优化 mqtt 客户端 增加虚拟线程支持

--- ruoyi-common/ruoyi-common-mqtt/src/main/java/org/dromara/common/mqtt/config/MqttAutoConfiguration.java
+++ ruoyi-common/ruoyi-common-mqtt/src/main/java/org/dromara/common/mqtt/config/MqttAutoConfiguration.java
@@ -3,9 +3,19 @@ package org.dromara.common.mqtt.config;
 import org.dromara.common.mqtt.listener.MqttClientConnectListener;
 import org.dromara.common.mqtt.listener.MqttClientGlobalMessageListener;
 import org.dromara.mica.mqtt.core.client.MqttClientCreator;
+import org.dromara.mica.mqtt.core.client.MqttClientCustomizer;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.VirtualThreadTaskExecutor;
+import org.tio.utils.thread.ThreadUtils;
+import org.tio.utils.thread.pool.SynThreadPoolExecutor;
+import org.tio.utils.thread.pool.TioCallerRunsPolicy;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * mqtt客户端配置初始化
@@ -29,6 +39,35 @@ public class MqttAutoConfiguration {
     @Bean
     public MqttClientGlobalMessageListener mqttClientGlobalMessageListener() {
         return new MqttClientGlobalMessageListener();
+    }
+
+    /**
+     * 客户端使用虚拟线程配置
+     */
+    @Bean
+    public MqttClientCustomizer mqttClientCustomizer() {
+        return creator -> {
+            // 这个数不重要 已经使用虚拟线程 就是填一下防止报错
+            int corePoolSize = ThreadUtils.CORE_POOL_SIZE;
+
+            ThreadFactory factory = new VirtualThreadTaskExecutor("tio-worker-virtual").getVirtualThreadFactory();
+            SynThreadPoolExecutor tioExecutor = new SynThreadPoolExecutor(corePoolSize, corePoolSize,
+                0L, new LinkedBlockingQueue<>(), factory, new TioCallerRunsPolicy());
+            tioExecutor.prestartCoreThread();
+            creator.tioExecutor(tioExecutor);
+
+            ThreadFactory factory1 = new VirtualThreadTaskExecutor("tio-group-virtual").getVirtualThreadFactory();
+            ThreadPoolExecutor groupExecutor = new ThreadPoolExecutor(corePoolSize, corePoolSize,
+                0L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), factory1, new TioCallerRunsPolicy());
+            groupExecutor.prestartCoreThread();
+            creator.groupExecutor(groupExecutor);
+
+            ThreadFactory factory2 = new VirtualThreadTaskExecutor("biz-worker-virtual").getVirtualThreadFactory();
+            ThreadPoolExecutor mqttExecutor = new ThreadPoolExecutor(corePoolSize, corePoolSize,
+                0L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), factory2, new TioCallerRunsPolicy());
+            mqttExecutor.prestartCoreThread();
+            creator.mqttExecutor(mqttExecutor);
+        };
     }
 
 }
