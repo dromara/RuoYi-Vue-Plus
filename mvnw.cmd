5d17aa185791fb0b56e9f11343cd594a7f9424ea
疯狂的狮子Li <15040126243@163.com>
2026-03-09 18:27:04 +0800

    update 优化 构建命令

--- mvnw.cmd
+++ mvnw.cmd
@@ -89,10 +89,12 @@ if (-not (Test-Path -Path $MAVEN_M2_PATH)) {
 }
 
 $MAVEN_WRAPPER_DISTS = $null
-if ((Get-Item $MAVEN_M2_PATH).Target[0] -eq $null) {
-  $MAVEN_WRAPPER_DISTS = "$MAVEN_M2_PATH/wrapper/dists"
+$mavenM2Item = Get-Item $MAVEN_M2_PATH
+$mavenM2Target = @($mavenM2Item.Target)
+if ($mavenM2Target.Count -gt 0 -and $null -ne $mavenM2Target[0]) {
+  $MAVEN_WRAPPER_DISTS = $mavenM2Target[0] + "/wrapper/dists"
 } else {
-  $MAVEN_WRAPPER_DISTS = (Get-Item $MAVEN_M2_PATH).Target[0] + "/wrapper/dists"
+  $MAVEN_WRAPPER_DISTS = "$MAVEN_M2_PATH/wrapper/dists"
 }
 
 $MAVEN_HOME_PARENT = "$MAVEN_WRAPPER_DISTS/$distributionUrlNameMain"
