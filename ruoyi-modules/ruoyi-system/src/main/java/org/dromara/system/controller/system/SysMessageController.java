ee2ee27cf1bcec1094bc771d0638b7915b706d65
疯狂的狮子Li <15040126243@163.com>
2026-03-27 18:26:36 +0800

    update 修改接口路径与cloud版本保持一致

--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/controller/system/SysMessageController.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/controller/system/SysMessageController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/system/message")
+@RequestMapping("/resource/message")
 public class SysMessageController extends BaseController {
 
     private final ISysMessageService messageService;
