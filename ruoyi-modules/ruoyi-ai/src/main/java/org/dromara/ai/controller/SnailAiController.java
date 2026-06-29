1fbe1694e4d2ba5b7ad69ce1e9d229a546df205e
疯狂的狮子Li <15040126243@163.com>
2026-06-29 18:14:35 +0800

    fix 修复 检查bean是否存在注解不生效问题

--- ruoyi-modules/ruoyi-ai/src/main/java/org/dromara/ai/controller/SnailAiController.java
+++ ruoyi-modules/ruoyi-ai/src/main/java/org/dromara/ai/controller/SnailAiController.java
@@ -11,7 +11,7 @@ import org.dromara.common.core.domain.R;
 import org.dromara.common.satoken.utils.LoginHelper;
 import org.dromara.common.web.core.BaseController;
 import org.dromara.system.api.model.LoginUser;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/snail-ai")
 @RequiredArgsConstructor
-@ConditionalOnBean(OpenApiUserClient.class)
+@ConditionalOnProperty(prefix = "snail-ai.open-api", name = "enabled", havingValue = "true")
 public class SnailAiController extends BaseController {
 
     /**
