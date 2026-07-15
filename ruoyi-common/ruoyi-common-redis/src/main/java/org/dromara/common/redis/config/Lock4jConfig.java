28c39d5b901beeddfccbb4c35c56032690b324a4
疯狂的狮子Li <15040126243@163.com>
2026-07-15 11:30:01 +0800

    update 优化 自动装配警告问题

--- ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/config/Lock4jConfig.java
+++ ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/config/Lock4jConfig.java
@@ -4,14 +4,17 @@ import cn.hutool.http.HttpStatus;
 import com.baomidou.lock.LockFailureStrategy;
 import com.baomidou.lock.spring.boot.autoconfigure.LockAutoConfiguration;
 import org.dromara.common.core.exception.ServiceException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Role;
 
 /**
  * Lock4j 配置
  *
  * @author AprilWind
  */
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 @AutoConfiguration(before = LockAutoConfiguration.class)
 public class Lock4jConfig {
 
@@ -19,6 +22,7 @@ public class Lock4jConfig {
      * Lock4j 获取锁失败策略。
      */
     @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public LockFailureStrategy defaultLockFailureStrategy() {
         return (key, method, arguments) -> {
             throw new ServiceException("业务处理中，请稍后再试...", HttpStatus.HTTP_UNAVAILABLE);
